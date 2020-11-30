f60582e78762ff030286544bbc515a073b254621
itorspwn <62305871+itorspwn@users.noreply.github.com>
2020-11-29 16:04:30 -0800

    cleaning up

--- static/js/app.js
+++ static/js/app.js
@@ -4,10 +4,8 @@ var tableData = data;
 // YOUR CODE HERE!
 // Define body
 var tablebody = d3.select("tbody");
-
 // Define filtered data 
 var filteredData = tableData;
-
 // Create function to load data
 function ShowData() {
     tablebody.html("");
@@ -23,17 +21,13 @@ function ShowData() {
 
 var button = d3.select("#filter-btn");
 button.on("click", runEnter);
-
 // Complete with run function
 function runEnter() {
-
     // Prevent the page from refreshing
     d3.event.preventDefault();
     var inputElement = d3.select("#datetime");
-
     var inputValue = inputElement.property("value");
-
-    if (inputValue) {
+  if (inputValue) {
         filteredData = filteredData.filter(date => date.datetime === inputValue);
         ShowData();
     }
