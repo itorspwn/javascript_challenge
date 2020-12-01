f6312f8b0cd0b1fbbf11161c4fe468f8f029775b
itorspwn <62305871+itorspwn@users.noreply.github.com>
2020-11-30 16:27:18 -0800

    adding notes

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,18 @@
 // import from data.js
 var tableData = data;
 
+//--------NOTES to Self---
+// BONUS: Refactor to use Arrow Functions!
+//data.forEach((weatherReport) => {
+  //var row = tbody.append("tr");
+ // Object.entries(weatherReport).forEach(([key, value]) => {
+    //var cell = row.append("td");
+   // cell.text(value);
+ // });
+//});
+//--------------
+
+
 // YOUR CODE HERE!
 // Define body
 var tablebody = d3.select("tbody");
@@ -9,9 +21,9 @@ var filteredData = tableData;
 // Create function to load data
 function ShowData() {
     tablebody.html("");
-    filteredData.forEach((insight) => {
+    filteredData.forEach((dataPoint) => {
     var row = tablebody.append("tr");
-    Object.entries(insight).forEach(([key, value]) => {
+    Object.entries(dataPoint).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
@@ -21,14 +33,14 @@ function ShowData() {
 
 var button = d3.select("#filter-btn");
 button.on("click", runEnter);
-// Complete with run function
+// Complete with run function when button is clicked
 function runEnter() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
-    var inputElement = d3.select("#datetime");
-    var inputValue = inputElement.property("value");
-  if (inputValue) {
-        filteredData = filteredData.filter(date => date.datetime === inputValue);
+    var element = d3.select("#datetime");
+    var value = element.property("value");
+  if (value) {
+        filteredData = filteredData.filter(date => date.datetime === value);
         ShowData();
     }
 }
